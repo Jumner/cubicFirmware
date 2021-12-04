18f9bb985948910a293cf2b7bb8e4294a9520016
JumnerArch <1frankjus@hdsb.ca>
2021-12-04 16:37:30 -0500

    messed with imu

--- arduino/final/main.ino
+++ arduino/final/main.ino
@@ -1,11 +1,25 @@
 #include "Motor.h"
 #include "Cubic.h"
 #include "PinChangeInterrupt.h"
+#include <Wire.h>
 Cubic cube;
 void setup()
 {
 	Serial.begin(9600);
+	Wire.begin(); // Join bus as master
+
 	delay(500);
+	Wire.beginTransmission(0b1101000); // Connect to mpu 6050
+	Wire.write(0x1b);
+	Wire.write(0b00000000); // Set gyro to +- 250 deg/s
+	// 000 00 000. 00 -> 250, 01 -> 500, 10 -> 1000, 11 -> 2000
+	Wire.endTransmission();
+
+	Wire.beginTransmission(0b1101000); // Connect to mpu 6050
+	Wire.write(0x1c);
+	Wire.write(0b00000000); // Set gyro to += 2g
+	// 000 00 000. 00 -> 2g, 01 -> 4g, 10 -> 8g, 11 -> 16g
+	Wire.endTransmission();
 	Serial.println("yo");
 
 	cube = Cubic();
@@ -35,3 +49,5 @@ void int2(void)
 {
 	cube.motors[2].interrupt();
 }
+
+//Mpu must be in main for some reason

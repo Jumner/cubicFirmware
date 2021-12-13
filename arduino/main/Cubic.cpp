68a437d9d53df249b60d73fceb6439c63eeabdb8
Jumner Chromebook <1frankjus@hdsb.ca>
2021-12-13 17:12:06 -0500

    finished everything

--- arduino/main/Cubic.cpp
+++ arduino/main/Cubic.cpp
@@ -60,9 +60,6 @@ BLA::Matrix<3, 9> Cubic::getK()
 	return {-7.2292e-08, -2.6568, -3.0579e-10, -0.0033118, -0.33108, -2.0317e-11, -0.00021725, 9.8982e-05, 9.8981e-05,
 					-7.2285e-08, 1.3284, 2.3013, -0.0033174, 0.16554, 0.28717, 9.8791e-05, -0.00021744, 9.8791e-05,
 					-7.2292e-08, 1.3284, -2.3013, -0.0033148, 0.16554, -0.28717, 9.8879e-05, 9.8879e-05, -0.00021735};
-	// return {-0.57735, -1144.7, 1.1682e-08, -9.8869, -142.66, 1.46e-09, -0.66664, 0.33336, 0.33336,
-	// 				-0.57735, 572.37, -992.73, -9.8869, 71.328, -123.88, 0.33336, -0.66664, 0.33336,
-	// 				-0.57735, 572.37, 992.73, -9.8869, 71.328, 123.88, 0.33336, 0.33336, -0.66664};
 }
 
 void Cubic::measureY(float t[3], VectorInt16 td)
@@ -77,12 +74,13 @@ void Cubic::calculateU()
 
 BLA::Matrix<9, 9> Cubic::getA()
 {
+	float x = mass * 9.81 * l / ix; // X Gravity
 	float y = mass * 9.81 * l / iy; // Y Gravity
 	float z = mass * 9.81 * l / iz; // Z Gravity
 	return {0, 0, 0, 1, 0, 0, 0, 0, 0,
 					0, 0, 0, 0, 1, 0, 0, 0, 0,
 					0, 0, 0, 0, 0, 1, 0, 0, 0,
-					0, 0, 0, 0, 0, 0, 0, 0, 0,
+					x, 0, 0, 0, 0, 0, 0, 0, 0,
 					0, y, 0, 0, 0, 0, 0, 0, 0,
 					0, 0, z, 0, 0, 0, 0, 0, 0,
 					0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -92,18 +90,16 @@ BLA::Matrix<9, 9> Cubic::getA()
 
 BLA::Matrix<9, 3> Cubic::getB()
 {
-	float w = 1 / iw;							 // Wheel acceleration
-	float x = -1 / (sqrt(3) * ix); // X Acceleration
-	float y = -2 / (sqrt(6) * iy); // Y Acceleration
-	float h = 1 / (sqrt(6) * iy);	 // Half Y Acceleration
-	float z = 1 / (sqrt(2) * iz);	 // Z Acceleration
-	float n = -1 / (sqrt(2) * iz); // Negative Z Acceleration (its acc positive)
+	float w = 1 / iw;	 // Wheel acceleration
+	float x = -1 / ix; // X Acceleration
+	float y = -1 / iy; // Y Acceleration
+	float z = -1 / iz; // Z Acceleration
 	return {0, 0, 0,
 					0, 0, 0,
 					0, 0, 0,
-					x, x, x,
-					y, h, h,
-					0, z, n,
+					x, 0, 0,
+					0, y, 0,
+					0, 0, z,
 					w, 0, 0,
 					0, w, 0,
 					0, 0, w};

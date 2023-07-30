399eae19452340efd4fb1b2bd788330b6410f981
Jumner ArchBtw <1frankjus@hdsb.ca>
2023-07-29 21:54:18 -0400

    crazy tuning +- 6 degree stability

--- julia/cubic.jl
+++ julia/cubic.jl
@@ -1,31 +1,3 @@
-# include("cubic.jl")
-# println("Starting...")
-# using ControlSystems
-# using Plots
-# using LinearAlgebra: I # Matrix(I,3,2)
-# using LaTeXStrings # L"\frac{1}{x}"
-# println("Libraries Loaded")
-
-# P = tf(1.0,[1,0.5,1])
-# A = []
-# B = []
-# C = []
-# D = []
-# P = ss()
-# println("System $P")
-# u(x,t) = [t>=10]
-# t = 0:0.1:30
-# x0 = [0.25,0]
-# result = lsim(P,u,t,x0=x0)
-# _, b, c, _ = result # y,t,x,u
-# display(plot(b,c', label=["Position" "Velocity"], xlabel="Time (s)"))
-
-# println("Done")
-
-# A\[1;2;3] solves the system
-
-# Sys
-
 using DifferentialEquations, Plots, REPL
 println("Loaded")
 
@@ -36,35 +8,20 @@ m = 1.219          # Mass of the pendulum bob (kg)
 I = 0.0233    # Moment of inertia of the pendulum bob (kg*m^2)
 Iw = 0.00025
 
+spErr = 0
+
 # System Dynamics
-# kp    = 14.7438 # (14.7438)
-# kd    = 0.458400 # 764kp/24573 (0.458400)
-# kw    = 0.000106228 # (0.000106206+0.000106250)/2
-# sp    = 0.00005094275 # (0.0000420729+0.0000598126)/2
-# kp = 15.05
-# kd = 5.185336
-# kw = 0.0008493
-# sp = 0.0008901
-# kp = 13.225482506
-# kd = 1.294508716
-# kw = 0.000177323
-# sp = 0.00042414
 
 # kp = 15.049274123133 # 0.0331144234778782
 # kd = 5.185336206253
 # kw = 0.000849371184
 # sp = 0.000890151154
 
-# Kp = 15.075736401053888
-# Kd = 4.990126644141603
-# Kw = 0.0008398183412843125
-# Sp = 0.0005199472146111858
+# kp = 15.075736401053888
+# kd = 4.990126644141603
+# kw = 0.0008398183412843125
+# sp = 0.0005199472146111858
 # SpErr = 0.03568700504400004
-
-kp = 11.33294219907 # 0.0658250119820011
-kd = 10.53018003246
-kw = 0.002297301121
-sp = 0.000971963255
 
 # Kp = 9.99445888247215
 # Kd = 10.065756059230512
@@ -76,10 +33,66 @@ sp = 0.000971963255
 # kw = 0.005044911892
 # sp = 0.1294959012023216
 
-# kp = 15
-# kd = 5
-# kw = 0.0008
-# sp = 0.0009
+# kp = 21.945828972632246
+# kd = 11.580674048834137
+# kw = 0.002679321645818333
+# sp = 0.0012945860846713632
+# Cost = 0.9364500629105373
+
+# Kp = 18.253995209053617
+# Kd = 7.256142775306458
+# Kw = 0.001377427684579893
+# Sp = 0.0007887638618331939
+# Cost = 1.2441001791549218
+
+# Kp = 19.49293017472906
+# Kd = 11.084590252251076
+# Kw = 0.002999999932901898
+# Sp = 0.0012999999339318308
+# Cost = 0.9288874084928395
+
+# kp = 19.579652663414134
+# kd = 11.263249388937721
+# kw = 0.002876207016294286
+# sp = 0.0013501617484196384
+# Cost = 0.9096206046795319
+
+# kp = 18.73245267140605
+# kd = 13.24602919045796
+# kw = 0.002603034897772071
+# sp = 0.001186421195111089
+# Cost = 0.8575023671451779
+
+# kp = 17.0206026929903
+# kd = 15.325630808390295
+# kw = 0.0034204833249101116
+# sp = 0.0013482366067321505
+# Cost = 0.7861798488238848
+
+
+# kp = 17.403088884098644
+# kd = 18.010258872222202
+# kw = 0.003538541810902399
+# sp = 0.0010994514647224955
+# Cost = 0.7859549575830388
+
+# kp = 19.28991079480639
+# kd = 21.268933356858117
+# kw = 0.0037808074309316916
+# sp = 0.0011749137653395208
+# Cost = 0.7602985281113408
+
+# kp = 21.16325927619272
+# kd = 20.569044424160897
+# kw = 0.004183753368447239
+# sp = 0.0015835614565139875
+# Cost = 0.7363852806102484
+
+kp = 22.317029661859724
+kd = 29.429799217979152
+kw = 0.005046194164776092
+sp = 0.001706491109572526
+# Cost = 0.6984172727932504
 ws = 1000 # Wheel scale
 function f(dθ, θ, p, t)
     
@@ -89,7 +102,7 @@ function f(dθ, θ, p, t)
     grav = m * g * L * sin(θ[1] + spErr - θ[4]) # Torque due to gravity
     # Control
     u = - kp * (θ[1] + spErr - w*kw - θ[4]) - kd * θ[2] - grav # 0.4584
-    u = max(min(u, torque), -torque)
+    u = max(min(u, max(0, torque)), -torque)
     dθ[1] = θ[2]
     dθ[2] = (u + m * g * L * sin(θ[1])) / I
     dθ[3] = u / (Iw * ws)
@@ -103,112 +116,78 @@ x0 = [0.0, 0.0, 0.0, 0.0]  # Initial state ([θ, θ_dot])
 # Time Span
 tspan = (0.0, 10.0)  # Simulation time span
 
+
 # Define the ODE Problem
 prob = ODEProblem(f, x0, tspan)
 
-# Solve the ODE
-
-function stable(sol)
-    last = sol[length(sol)]
-    if abs(last[1]) > 0.01
-        return false
-    elseif abs(last[2]) > 0.1
-        return false
-    elseif abs(last[3] * ws) > 5
-        return false
-    end
-    return true
-end
-
 sol = solve(prob)
-function kLimit(n, digits, max=1) # 0->kp, 1->kd, 2->kw, 3->sp, 4->spErr, Max is 1 if max -1 if min
-    if n == 0
-        global spErr = 0.0001
-    end
-    if (digits < 0)
-        return
-    end
-    kLimit(n, digits-1, max)
-    i = 0
-    while true
-        global sol = solve(prob)
-        i = i + 1
-        val = 10.0^-digits * max * (stable(sol)*2-1) # Flip for min or when we overshoot
-        if n == 0
-            global spErr += val
-        elseif n == 1
-            global kp += val
-        elseif n == 2
-            global kd += val
-        elseif n == 3
-            global kw += val
-        elseif n == 4
-            global sp += val
-        end
-        if !stable(sol)
-            break
-        end
-    end
-    if n == 0
-        return spErr
-    elseif n == 1
-        return kp
-    elseif n == 2
-        return kd
-    elseif n == 3
-        return kw
-    elseif n == 4
-        return sp
-    end
-end
-
-oldSpErr = 0
-
+oldCost = 1
 spErrPrec = 9
 
-function grad(nudge = 0.001)
+function cost()
+    global spErr = 0.02
+    global sol = solve(prob)
+    c = 0.0
+    weights = [6922.24, 364.81, 0.00117649, 571.21]
+    for i = 2:length(sol)
+        deltaT = sol.t[i] - sol.t[i-1]
+        c += (sol.u[i][1])^2 * weights[1] * deltaT
+        c += (sol.u[i][2])^2 * weights[2] * deltaT
+        c += (sol.u[i][3] * ws)^2 * weights[3] * deltaT
+        c += (sol.u[i][4] - spErr)^2 * weights[4] * deltaT
+    end
+    return c
+end
+
+oldKp = 0
+oldKd = 0
+oldKw = 0
+oldSp = 0
+function grad(nudge = 0.1)
+    global oldKp = kp
+    global oldKd = kd
+    global oldKw = kw
+    global oldSp = sp
     gradient = [0.0, 0.0, 0.0, 0.0]
+    global oldCost = cost()
 
-    global oldSpErr = kLimit(0,spErrPrec)
-    global kp += nudge
-    gradient[1] = (kLimit(0,spErrPrec) - oldSpErr)/nudge
-    global kp -= nudge
+    global kp += nudge * kp
+    gradient[1] = (cost() - oldCost)/(nudge * kp)
+    global kp = oldKp
 
-    global oldSpErr = kLimit(0,spErrPrec)
-    global kd += nudge
-    gradient[2] = (kLimit(0,spErrPrec) - oldSpErr)/nudge
-    global kd -= nudge
+    global kd += nudge * kd
+    gradient[2] = (cost() - oldCost)/(nudge * kd)
+    global kd = oldKd
 
-    global oldSpErr = kLimit(0,spErrPrec)
-    global kw += nudge
-    gradient[3] = (kLimit(0,spErrPrec) - oldSpErr)/nudge
-    global kw -= nudge
+    global kw += nudge * kw
+    gradient[3] = (cost() - oldCost)/(nudge * kw)
+    global kw = oldKw
 
-    global oldSpErr = kLimit(0,spErrPrec)
-    global sp += nudge
-    gradient[4] = (kLimit(0,spErrPrec) - oldSpErr)/nudge
-    global sp -= nudge
+    global sp += nudge * sp
+    gradient[4] = (cost() - oldCost)/(nudge * sp)
+    global sp = oldSp
 
-    return gradient
+    return -gradient
 end
 
 function ascent(learningRate)
-    gradient = grad(0.0001)
+    gradient = grad(learningRate*100)
     global kp += learningRate / gradient[1]
     global kd += learningRate / gradient[2]
     global kw += learningRate / gradient[3]
     global sp += learningRate / gradient[4]
-    print("Kp = ")
+    print("kp = ")
     println(kp)
-    print("Kd = ")
+    print("kd = ")
     println(kd)
-    print("Kw = ")
+    print("kw = ")
     println(kw)
-    print("Sp = ")
+    print("sp = ")
     println(sp)
-    print("SpErr = ")
-    println(kLimit(0,spErrPrec))
-    return spErr/oldSpErr - 1
+    c = cost()
+    print("Cost = ")
+    println(c)
+    return oldCost/c - 1
 end
 function getch()
     ret = ccall(:jl_tty_set_mode, Int32, (Ptr{Cvoid},Int32), stdin.handle, true)
@@ -218,23 +197,27 @@ function getch()
     c
 end
 
-rate = 0.00001
+rate = 0.001
 while true
+    print("Cost = ")
+    println(cost())
     println("Press any key to continue or (q)uit")
     if getch() == 'q'
         break
     end
     for i=1:100
         increase = ascent(rate)
-        if increase < 0
-            global rate /= 2.0
-        elseif increase < 0.001
-            global rate *= 1.1
+        if increase < 0.0
+            global rate /= 200.0
+            global kp = oldKp
+            global kd = oldKd
+            global kw = oldKw
+            global sp = oldSp
+        else
+            global rate *= 50.0
         end
-        print(round(100*increase; digits=2))
+        print(round(100*increase; digits=4))
         println("% Increase")
-        # global spErr = 0.02
-        p = plot(solve(prob))
-        display(p)
+        display(plot(sol))
     end
 end
